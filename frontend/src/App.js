6ea448ef274b27344477b73e0359afd8d5bcd6d9
ahmedhabeeb73 <alnaib888@gmail.com>
2020-09-30 17:53:55 +0200

    add header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from 'react-bootstrap';
 
 
 const App = () => {
   return (
     <>
+    <Header/>
+    <main className="py-3">
+      <Container>
      <h1>welcome to Dundung app</h1>
+      </Container>
+     </main>
+     <Footer/>
     </>
   );
 }
