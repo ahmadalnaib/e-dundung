27ee996eaa6afa3730f641432f2762c28fdccb98
ahmedhabeeb73 <alnaib888@gmail.com>
2020-09-30 19:08:48 +0200

    add homescreen product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 import {Container} from 'react-bootstrap';
 
 
@@ -11,6 +12,7 @@ const App = () => {
     <main className="py-3">
       <Container>
      <h1>welcome to Dundung app</h1>
+     <HomeScreen/>
       </Container>
      </main>
      <Footer/>
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Container,Row,Col} from 'react-bootstrap';
 
 
-function Footer() {
+const Footer = () => {
   return <footer>
     <Container>
       <Row>
